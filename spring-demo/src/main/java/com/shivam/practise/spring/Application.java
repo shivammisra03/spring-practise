322d298021ef437b0e5e6ce4e8407c857314d090
shivam misra <shivam.misra@>
2018-06-10 18:34:55 +0530

    Added class files

---
+++ spring-demo/src/main/java/com/shivam/practise/spring/Application.java
@@ -0,0 +1,13 @@
+package com.shivam.practise.spring;
+
+public class Application {
+	public static void main(String[] args) {
+		Triangle triangle = new Triangle();
+		triangle.draw();
+		
+		
+		
+		
+	}
+
+}
---
+++ spring-demo/src/main/java/com/shivam/practise/spring/Triangle.java
@@ -0,0 +1,17 @@
+package com.shivam.practise.spring;
+
+public class Triangle {
+	private String type;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	public void draw(){
+		System.out.println("Triangle drawn");
+	}
+
+}
