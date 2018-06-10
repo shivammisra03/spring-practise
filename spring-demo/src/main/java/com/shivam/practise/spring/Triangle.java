8c2a939544ce609eeecf8c2b8c45bceee9ad3aa0
shivam misra <shivam.misra@>
2018-06-10 19:05:34 +0530

    Object dependancy

--- spring-demo/src/main/java/com/shivam/practise/spring/Triangle.java
+++ spring-demo/src/main/java/com/shivam/practise/spring/Triangle.java
@@ -1,17 +1,36 @@
 package com.shivam.practise.spring;
 
 public class Triangle {
-	private String type;
-
-	public String getType() {
-		return type;
+	
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	public Point getPointA() {
+		return pointA;
 	}
-
-	public void setType(String type) {
-		this.type = type;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
-	public void draw(){
-		System.out.println("Triangle drawn");
+	public Point getPointB() {
+		return pointB;
 	}
-
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+	public Point getPointC() {
+		return pointC;
+	}
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+	
+	public void draw() {
+		System.out.println("POINT A : {"+this.pointA.getX()+" , "+this.pointA.getY()+"}");
+		System.out.println("POINT B : {"+this.pointB.getX()+" , "+this.pointB.getY()+"}");
+		System.out.println("POINT C : {"+this.pointC.getX()+" , "+this.pointC.getY()+"}");
+		
+	}
+	
+	
+	
 }
