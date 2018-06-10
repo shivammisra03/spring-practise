e8413b547b9c4e6b0ad164eaafd4a4190280025b
shivam misra <shivam.misra@>
2018-06-10 18:37:51 +0530

    Updated ApplicationContext code

--- spring-demo/src/main/java/com/shivam/practise/spring/Application.java
+++ spring-demo/src/main/java/com/shivam/practise/spring/Application.java
@@ -1,12 +1,15 @@
 package com.shivam.practise.spring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Application {
 	public static void main(String[] args) {
-		Triangle triangle = new Triangle();
+//		Triangle triangle = new Triangle();
+		
+		ApplicationContext beanFactory = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle)beanFactory.getBean("triangle");
 		triangle.draw();
-		
-		
-		
 		
 	}
 
